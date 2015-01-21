c86613f3ae2ee86446dba1ae336f2a9d3a931788
Tuzkiss <tuzkiss1017@gmail.com>
2015-01-21 18:52:18 +0800

    Signed-off-by: Tuzkiss <tuzkiss1017@gmail.com>

--- scripts/index.js
+++ scripts/index.js
@@ -14,12 +14,85 @@ require.config ({
 });
 
 // require js
-requirejs ( ['localStorage'], function ( ls ) {
-	//ls.showStorage();
-	//console.log( ls );
-	//ls.setStorage('fudaye', 'nihao2~');
-	var a = ls.showStorage();
+requirejs ( ['profile', 'background'], function ( tuzkiss, bg ) {
+	
+	//bg.setBackground();
 
-	console.log(a);
+            var canvas = document.getElementsByTagName('canvas')[0],
+                ctx = canvas.getContext('2d'),
+                pr = window.devicePixelRatio || 1,  // get the device pixel ratio
+                winW = document.body.offsetWidth || document.documentElement.offsetWidth,
+                winH = document.body.offsetHeight || document.documentElement.offsetHeight,
+                f = 90,
+                posArray = [],
+                radian = 0, // 弧度
+                pi = Math.PI,
+                rand = Math.random;
+
+            winH -= 10;
+            canvas.width = winW;   
+            canvas.height = winH; 
+
+            ctx.globalAlpha = 0.6;
+
+            var drawBackground = function () {
+                ctx.clearRect(0, 0, winW, winH)
+
+                posArray = [{
+	                	x : 0,
+	                	y : winH * .7 + f
+	                } ,
+	                {
+	                	x : 0,
+	                	y : winH * .7 - f
+	                }
+	            ];
+	            console.log(posArray);
+
+                if( posArray[1].x < winW + f ) 
+                	draw(posArray[0], posArray[1]);
+            }
+
+            var draw = function (i, j) {   
+                
+                ctx.beginPath();
+                ctx.moveTo(i.x, i.y);
+                ctx.lineTo(j.x, j.y);
+
+                var k = j.x + ( rand() * 2 - 0.25 ) * f,  n = y(j.y);
+                console.log(k + ' ' + n);
+
+                ctx.lineTo(k, n);
+                ctx.closePath();
+
+                radian -= ( pi * 2 / -50);
+
+                var color = ( Math.cos(radian) * 127 + 128 << 16 | Math.cos( radian + pi * 2 / 3 ) * 127 + 128 << 8 | Math.cos( radian + pi * 2 / 3 * 2) * 127 + 128).toString(16);
+
+                console.log(color);
+                ctx.fillStyle = '#'+ color ;
+                ctx.fill();
+
+                // change the posArray
+                posArray[0] = posArray[1];
+                posArray[1] = { x:k, y:n };
+            }
+
+            var y = function (p) {
+                var t = p + ( rand() * 2 - 1.1) * f;
+                return (t > winH || t < 0) ? y(p) : t;
+            }
+
+
+            document.onclick = drawBackground;
+            document.ontouchstart = drawBackground;
+            drawBackground();
+
+            document.addEventListener('touchmove', function (e) {
+                e.preventDefault();
+            });
+
+
+	
 });
 
---
+++ scripts/profile.js
@@ -0,0 +1,26 @@
+/**
+*	 Copyright (c) 2014 - 2015 TuzK1ss 
+*
+*  				profile.js
+*
+*     last modify date on 2015-1-16
+*/
+
+define(function () {
+	var tuzkiss = {
+		name 		: 	'',
+		pinyin 		: 	'jianqing ding',
+		ename 		: 	'tuzkiss', 
+		university  : 	'Zhejiang Ocean University',
+		major 		: 	'computer science and technology',
+		skills 		: 	[],
+		bobby		:   [],
+		goal		:   'Full Stack Developer'
+	};
+
+	tuzkiss.skills.push('HTML5');
+
+	return tuzkiss;
+});
+
+
