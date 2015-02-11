8c0e1491d08aa68bf24d866a303cbb125c650d11
Tuzkiss <tuzkiss1017@gmail.com>
2015-02-11 18:09:13 +0800

    tuzkiss:upadte the profile

--- scripts/profile.js
+++ scripts/profile.js
@@ -6,6 +6,64 @@
 *     last modify date on 2015-2-5
 */
 
+var profile = {};
 
-console.log('load profile success.');
+window.onload = function () {
+	profile.bindClick();
+
+	profile.replaceInfo();
+};
+
+// bind the li click event
+profile.bindClick = function () {
+	'use strict';
+
+	var li = document.querySelectorAll('nav>ul>li');
+
+	for (var i = 0; i < li.length; i ++) {
+		(function (i) {
+			li[i].addEventListener('click', function () {
+				var activePanel = document.querySelector('.active-panel'),
+					activeLi 	= document.querySelector('.active'),
+					panel 		= document.querySelectorAll('.panel');
+
+					activeLi.className = activeLi.className.replace('active', '');
+					li[i].className += 'active';
+
+				if (activePanel) {
+					 
+					 activePanel.className = activePanel.className.replace('active-panel', 'hidden');;
+
+					 panel[i].className = panel[i].className.replace('hidden', 'active-panel');
+				};
+
+				profile.replaceInfo();
+			});
+		})(i);
+	};
+};
+
+// replace info html 
+profile.replaceInfo = function () {
+	'use strict';
+
+	var activePanel 	= document.querySelector('.active-panel'),
+		replaceArray 	= activePanel.innerHTML.match(/{{\w+}}/g),
+		replaceName,
+		replaceValue;
+	
+	if (replaceArray) {
+		for (var i = 0; i < replaceArray.length; i ++) {
+			replaceName 	= replaceArray[i];
+			replaceValue 	= tuzkiss[replaceName.substring(2, replaceName.length - 2)];
+
+			if (replaceValue) {
+				activePanel.innerHTML = activePanel.innerHTML.replace(replaceName, replaceValue);
+			} else {
+				activePanel.innerHTML = activePanel.innerHTML.replace(replaceName, 'null');
+			};
+		};
+	};
+
+};
 
--- scripts/profileInfo.js
+++ scripts/profileInfo.js
@@ -1,16 +1,15 @@
+/* tuzkiss infomation */
+
+var tuzkiss = {
+	name 		: 	'tuzkiss',
+	pinyin 		: 	'jianqing ding',
+	university  : 	'Zhejiang Ocean University',
+	major 		: 	'computer science and technology',
+	skills 		: 	[],
+	hobby		:   [],
+	goal		:   'Full Stack Developer'
+};
+
+tuzkiss.skills.push('HTML5');
 
 
-define(function () {
-	var tuzkiss = {
-		name 		: 	'tuzkiss',
-		pinyin 		: 	'jianqing ding',
-		university  : 	'Zhejiang Ocean University',
-		major 		: 	'computer science and technology',
-		skills 		: 	[],
-		hobby		:   [],
-		goal		:   'Full Stack Developer'
-	};
-
-	tuzkiss.skills.push('HTML5');
-
-	return tuzkiss;
