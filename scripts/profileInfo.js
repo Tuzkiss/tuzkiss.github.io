905f674f07528f51b1444615211c7656569168e1
Tuzkiss <Tuzkiss1017@gmail.com>
2015-02-28 18:34:48 +0800

    upadte my proflie

--- scripts/profileInfo.js
+++ scripts/profileInfo.js
@@ -7,7 +7,20 @@ var tuzkiss = {
 	major 		: 	'computer science and technology',
 	skills 		: 	[],
 	hobby		:   [],
-	goal		:   'Full Stack Developer'
+	goal		:   'Full Stack Developer',
+
+	QQ			:   '245361935',
+	mobile 		:   '18658550504',
+	Email		:   'Tuzkiss1017#gmail.com',
+	github		:   'https://github.com/Tuzkiss',
+	CSDN		:   'http://my.csdn.net/tuzkiss1017',
+	blog		:   'http://blog.csdn.net/tuzkiss1017',
+	weibo		: 	'http://weibo.com/tuzkiss',
+	twitter     :   '@TuzK1ss',
+	facebook	:   '@TuzK1ss'
+
+
+
 };
 
 tuzkiss.skills.push('HTML5');
