d79a8182f3b46874eb90141171bcfef742d93e3a
Tuzkiss <Tuzkiss1017@gmail.com>
2015-03-23 21:55:04 +0800

    update

--- scripts/profileInfo.js
+++ scripts/profileInfo.js
@@ -1,6 +1,8 @@
 /* tuzkiss infomation */
 
-var tuzkiss = {
+var tuzkissEn = {
+
+	// base info
 	name 		: 	'tuzkiss',	
 	pinyin 		: 	'jianqing ding',
 	age			:   '24',
@@ -14,8 +16,12 @@ var tuzkiss = {
 	major 		: 	'Computer science and technology',
 	skills 		: 	[],
 	hobby		:   'Football, Surf the Internet, Listen to music and more ...',
-	goal		:   'Full Stack Developer',
+	goal		:   'Front End Develper -> Full Stack Developer',
 
+
+
+
+	// contact info
 	QQ			:   '245361935',
 	mobile 		:   '18658550504',
 	Email		:   'Tuzkiss1017#gmail.com',
@@ -30,9 +36,86 @@ var tuzkiss = {
 
 };
 
-tuzkiss.hobby.push('football');
-tuzkiss.hobby.push('');
+tuzkissEn.skills.push('HTML5');
+tuzkissEn.skills.push('CSS3');
+
+
+var tuzkiss = {
+
+	// base info
+	name 		: 	'tuzkiss',	
+	pinyin 		: 	'丁建强',
+	age			:   '24',
+	birthday	:   '1992-10-17',
+	location	:   '浙江杭州',
+	gallery1	:   '<img src="images/gallery1.jpg" height="200" />',
+	gallery2	:   '<img src="images/gallery2.jpg" height="200" />',
+	gallery3	:   '<img src="images/gallery3.jpg" height="200" />',
+	university  : 	'浙江海洋学院',
+	university_logo : '<img src="images/school_logo.jpg" width="20" height="20" />',
+	major 		: 	'计算机科学与技术',
+	skills 		: 	[],
+	hobby		:   '足球，上网，听音乐，阅读等等...',
+	goal		:   '前端攻城狮  -> 全端工程师',
+
+	// skill info
+
+	// prize info
+
+	// university experence info
+
+	// project experence info
+	projectExperence: [],
+	
+	// internship experence info
+	internshipExperence : [],
+
+	// contact info
+	QQ			:   '245361935',
+	mobile 		:   '18658550504',
+	Email		:   'Tuzkiss1017#gmail.com',
+	github		:   'https://github.com/Tuzkiss',
+	CSDN		:   'http://my.csdn.net/tuzkiss1017',
+	blog		:   'http://blog.csdn.net/tuzkiss1017',
+	weibo		: 	'http://weibo.com/tuzkiss',
+	twitter     :   '@TuzK1ss',
+	facebook	:   '@TuzK1ss'
+
+
+
+};
+
 tuzkiss.skills.push('HTML5');
 tuzkiss.skills.push('CSS3');
 
+tuzkiss.projectExperence.push({
+	date:'2012年7月',
+	project:'为校海岛野外生存团队制作高等教育国家级教学成果申报网站',
+	skill: 'HTML + CSS + JAVASCRIPT',
+	experenece:'第一次制作整站，开始慢慢接触HTML,CSS,JAVASCRIPT等，并对网站制作产生浓厚兴趣'
+});
 
+tuzkiss.projectExperence.push({
+	date:' 2013年10月',
+	project:'为学校学生社团联合会开发校社联官方网站',
+	skill: 'HTML + CSS + JavaScript + ASP.NET4.0 + ACCESS+jQuery',
+	experenece: '第一次开发动态网站，了解整个网站的流程，运行，以及底层实现。开始慢慢有往前端的意识，因为更热衷于做让人愉悦的页面和动画',
+	websiteUrl: 'http://xsl.zjou.edu.cn/'
+});
+
+tuzkiss.projectExperence.push({
+	date:' 2014年5月',
+	project:'组织团队开发分布式图片处理系统参加浙大网新组织的服务外包比赛',
+	skill: 'Gearman + OpenCV + PHP + HTML + CSS ',
+	experenece: '在项目中担任项目经理一职，主要负责Gearman环境搭建，核心代码编写，统筹项目成员任务分配，并负责决赛答辩。在该项目中，学会了管理一个团队来完成一个项目，同时也学会使用Cygwin和Linux的一些基本操作，初步对分布式系统有了了解，系统的整合成一个分布式图片处理系统'
+});
+
+
+tuzkiss.internshipExperence.push({
+	time:' 2014年7月到至今',
+	project:'.NET COE',
+	skill: '.NET , HTML, CSS, JAVASCRIPT等',
+	experenece: '在实习期间，主要负责的是组内的系统前端改进，性能优化，后端BUG修复。从2015年2月开始，在新的项目组里主要担任前端页面、组件开发以及与.NET MVC链接获取后端WCF传递数据。' +
+				'在这几个月的时间里，慢慢的开始把学习工作重心从后端转移到了前端，或许是因为我开始慢慢了解自己喜欢的也许更多的前端，因为自己会沉浸于为系统开发了一个简单实用的自定义组件，' +
+				'也因为在前端学习中获得一点代码，性能的优化而开心满怀。感谢实习，感谢时间让我明白自己想做的事情。这或许就是这段时间来最大的收获吧！'
+});
